ed7f44095ce97da3789e3cf590886e0a67abb1dd
Victor Bittorf <bitfort@gmail.com>
2012-04-15 16:49:19 -0500

    added net tool and working

---
+++ nettools/nettool.py
@@ -0,0 +1,20 @@
+"""
+Discover information about your network
+"""
+
+
+import subprocess
+
+def get_raw_trace():
+  txt = subprocess.check_output(["/usr/sbin/traceroute", '-m', '5', 
+                                 'www.google.com'], stderr=subprocess.PIPE)
+  lst = []
+  for l in txt.split('\n')[:5]:
+    stuff = l.split(' ')
+    stuff = filter(lambda x: len(x) > 0, stuff)
+    num, host, ip = tuple(stuff[:3])
+    lst.append((host, ip))
+  return lst
+
+print get_raw_trace()
+
